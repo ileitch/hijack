760537e22bc8e0401ad9c7dbc3cd88b803266c5b
Ian Leitch <ian.leitch@systino.net>
2009-07-09 21:33:11 -0400

    Added README

--- example/example.rb
+++ example/example.rb
@@ -1,34 +1,20 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'hijack'
 
-  puts <<-EOS
-Welcome to Hijack!
-==================
+NAME = 'Example'
 
-This example demonstrates how to use Hijack in your long-running apps.
-All you need to do is call Hijack.provide(self) before your app enters its main loop,
-you'll then be able to connect to the app via the command 'hijack <PID>'.
-Instead of using the process ID to connect with, you can instead give your Hijacked app a name,
-for example: Hijack.provide(self, :name => 'HijackExample').
+Hijack.provide(self, :name => NAME)
 
-Open up a new terminal window and run 'hijack Example' to connect to this process.
-You'll then have access to the context of this app just as if it were a local IRB session!
 
-Example:
+puts <<-EOS
+Welcome to the Hijack example!
+
+You can now connect to this process, e.g:
 
 $ hijack Example
 => Hijacked Example (example.rb) (ruby 1.8.7 [i686-darwin9])
->> puts self
-main
-=> nil
->> puts Process.pid
-1234
-=> nil
->> 1 + 1
-=> 2
+>> 
 EOS
-
-Hijack.provide(self, :name => 'Example')
 
 while true
   sleep 1
