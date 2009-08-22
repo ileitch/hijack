267b6a5668290521d11c67deeef08389f046a7ba
Ian Leitch <ian.leitch@systino.net>
2009-08-21 21:11:39 -0400

    Added an example script to demonstrate hijacking ActionController's dispatcher to print basic request info

---
+++ examples/rails_dispatcher.rb
@@ -0,0 +1,13 @@
+ActionController::Dispatcher.class_eval do
+  class << self
+    def dispatch_with_spying(cgi, session_options, output)
+      env = cgi.__send__(:env_table)
+      puts "#{Time.now.strftime('%Y/%m/%d %H:%M:%S')} - #{env['REMOTE_ADDR']} - #{env['REQUEST_URI']}"
+      dispatch_without_spying(cgi, session_options, output)
+    end
+
+    alias_method :dispatch_without_spying, :dispatch
+    alias_method :dispatch, :dispatch_with_spying
+  end
+end
+nil
